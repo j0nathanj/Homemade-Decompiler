8337d714f7f81e802af68f40a508870c5496a231
Shahaf Cohen Tarica <shahafco24@gmail.com>
2018-03-17 19:30:32 +0200

    Renamed classes, added abstract AsmFile class to manage as files and subclass AsmElfFile(AsmFile) to manage asm files from elf files

--- src/AsmParser.py
+++ src/AsmParser.py
@@ -2,77 +2,112 @@ import pwn
 import re
 
 
+class AsmFile(object):
+    """
+    Abstract class to describe an ASM file.
+    The inheriting subclasses should receive only a binary file name and disassemble it in order to implement this
+    class' functions.
+    """
+
+    def __init__(self, bin_filename):
+        pass
+
+    def get_func_address_size_and_content(self, func_name):
+        """
+        Get the address, size and content of the function.
+
+        :param func_name: The function name
+        :type func_name: str
+        :return: The address of the function, its size (in bytes) and its content
+        :rtype: tuple(int, int, str)
+        """
+        pass
+
+    def get_function(self, func_name):
+        """
+        Get an AsmFunction object describing the function.
+
+        :param func_name: The function name
+        :type func_name: str
+        :return: AsmFunction object of the function
+        :rtype: AsmFunction
+        """
+        pass
+
+
+class AsmElfFile(AsmFile):
+    def __init__(self, elf_filename):
+        """
+        Initiate the ELF file name and pwn.ELF object.
+
+        :param elf_filename: The ELF filename
+        :type elf_filename: str
+        """
+        super(AsmElfFile, self).__init__(elf_filename)
+        self._filename = elf_filename
+        self._file = pwn.ELF(elf_filename)
+
+    def get_func_address_size_and_content(self, func_name):
+        func = self._file.functions[func_name]
+        return func.address, func.size
+
+    def get_function(self, func_name):
+        return AsmFunction(self, func_name)
+
+
 class AsmFunction(object):
     def __init__(self, src_file, function_name):
         """
         Initiate the assembly function file, name, address, size and instructions.
+
         :param src_file: The ELF file from which to take the function
         :param function_name: The function name
-
-        :type src_file: str
+        :type src_file: AsmFile
         :type function_name: str
         """
         self._src_file = src_file
         self._name = function_name
-        self._address, self._size, self._content = self.get_func_by_name(src_file, function_name)
+        self._address, self._size, self._content = src_file.get_func_address_size_and_content(function_name)
         self._instructions = self.func_content_to_instruction_arr(self._content)
-
-    @staticmethod
-    def get_func_by_name(elf_file, func_name):
-        """
-        Get the function's address, size and instructions (as string).
-        :param elf_file: The ELF file from which to take the function
-        :param func_name: The function name
-
-        :type elf_file: str
-        :type func_name: str
-
-        :return: function address, function size, function instructions
-        :rtype: tuple(int, int, str)
-        """
-        e = pwn.ELF(elf_file)
-        func = e.functions[func_name]
-        return func.address, func.size, e.disasm(func.address, func.size)
 
     @staticmethod
     def func_content_to_instruction_arr(func_content):
         """
         Convert the instructions string into a list of InstructionLine
+
         :param func_content: The instructions
-
         :type func_content: str
-
         :return: The corresponding list of InstructionLine
-        :rtype: list[InstructionLine]
+        :rtype: list[AsmInstruction]
         """
         instructions = []
         for line in func_content.split('\n'):
             try:
-                inst = InstructionLine(line)
+                inst = AsmInstruction(line)
                 instructions.append(inst)
-            except InvalidInstrucionLineException as err:
+            except InvalidInstructionLineException as err:
                 print err
         return instructions
 
 
-class InstructionLine(object):
+class AsmInstruction(object):
     def __init__(self, line):
         """
         Initiate the assembly instruction line address, command and comment address.
 
         If the command line doesn't contain a command, an exception is raised.
-        :param line: The command line according to pwnlib.disasm output format
 
+        :param line: The command line according to pwnlib.disasm output format
         :type line: str
         """
         command_pattern = ' *([0-9a-f]+): *(?:[0-9a-f]{2} ){1,7} *(.+)'
         if not re.match(command_pattern, line):
-            raise InvalidInstrucionLineException(line, 'Invalid instruction pattern')
+            raise InvalidInstructionLineException(line, 'Invalid instruction pattern')
 
         temp_line = re.sub(command_pattern, '\\1:\\2', line)
 
-        self._address, self._command = temp_line.split(':')
-        self._address = int('0x' + self._address, 16)
+        address_str, self._command = temp_line.split(':')
+        self._address = int('0x' + address_str, 16)
         self._command = self._command.strip()
         if '# 0x' in line:  # In case of addresses in the binary file
             self._comment_address = int('0x' + line.split('# 0x')[-1], 16)
@@ -80,7 +115,7 @@ class InstructionLine(object):
             self._comment_address = None
 
 
-class InvalidInstrucionLineException(Exception):
+class InvalidInstructionLineException(Exception):
     def __init__(self, instruction_line, reason=None):
         self._instruction_line = instruction_line
         self._reason = reason
@@ -100,7 +135,7 @@ if __name__ == '__main__':
     for l in params.split('\n'):
         try:
             print l
-            ins = InstructionLine(l)
+            ins = AsmInstruction(l)
             print "Address: {} \n Command: {}".format(ins._address, ins._command)
         except Exception as err:
             print err
